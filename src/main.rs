cdd7097bfabf18e99246da9661a6f0d96df39f22
Werner Laurensse <werner@alman.ax>
2021-08-14 19:08:29 +0200

    Refactor create_pipeline

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,18 @@
 use std::{fs};
-use std::path::PathBuf;
+use std::path::{PathBuf,Path};
 use std::sync::mpsc::channel;
 use winit::{
     event::*,
     event_loop::{ControlFlow, EventLoop, EventLoopProxy},
     window::{WindowBuilder, Window}};
-use wgpu;
 use pollster;
 use notify::{RawEvent, RecommendedWatcher, Watcher};
+use naga::{front::wgsl, valid::{ValidationFlags, Validator, Capabilities}};
 
 struct ReloadEvent;
 
 struct State {
+    // window: Window,
     surface: wgpu::Surface,
     device: wgpu::Device,
     queue: wgpu::Queue,
@@ -19,11 +20,15 @@ struct State {
     swap_chain: wgpu::SwapChain,
     size: winit::dpi::PhysicalSize<u32>,
     render_pipeline: wgpu::RenderPipeline,
+
+    fragment_path: PathBuf,
+    validator: Validator,
+    vertex_shader: wgpu::ShaderModule,
 }
 
 impl State {
     // Creating some of the wgpu types requires async code
-    async fn new(window: &Window) -> Self {
+    async fn new(window: &Window, fragment_path: &Path) -> Self {
         let size = window.inner_size();
 
         let instance = wgpu::Instance::new(wgpu::BackendBit::PRIMARY);
@@ -40,9 +45,10 @@ impl State {
                 label: None},
             None,
         ).await.unwrap();
+        let swapchain_format = adapter.get_swap_chain_preferred_format(&surface).unwrap();
         let sc_desc = wgpu::SwapChainDescriptor {
             usage: wgpu::TextureUsage::RENDER_ATTACHMENT,
-            format: adapter.get_swap_chain_preferred_format(&surface).unwrap(),
+            format: swapchain_format,
             width: size.width,
             height: size.height,
             present_mode: wgpu::PresentMode::Fifo}; // Faster framerate with Immediate or Mailbox but this is not optimal for mobile... 
@@ -52,50 +58,14 @@ impl State {
             label: Some("Vertex Shader"),
             flags: wgpu::ShaderFlags::all(),
             source: wgpu::ShaderSource::Wgsl(include_str!("./vertex.wgsl").into())});
-        let fragment_shader = device.create_shader_module(&wgpu::ShaderModuleDescriptor {
-            label: Some("Fragment Shader"),
-            flags: wgpu::ShaderFlags::all(),
-            source: wgpu::ShaderSource::Wgsl(include_str!("./demo.wgsl").into())});
         let render_pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
             label: Some("Render Pipeline Layout"),
             bind_group_layouts: &[], // TODO add bind group for uniform buffer with screen resolution
             push_constant_ranges: &[]});
-        let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
-            label: Some("Render Pipeline"),
-            layout: Some(&render_pipeline_layout),
-            vertex: wgpu::VertexState {
-                module: &vertex_shader,
-                entry_point: "main",
-                buffers: &[],
-            },
-            fragment: Some(wgpu::FragmentState {
-                module: &fragment_shader,
-                entry_point: "main",
-                targets: &[wgpu::ColorTargetState {
-                    format: sc_desc.format,
-                    blend: Some(wgpu::BlendState::REPLACE),
-                    write_mask: wgpu::ColorWrite::ALL,
-                }],
-            }),
-            primitive: wgpu::PrimitiveState {
-                topology: wgpu::PrimitiveTopology::TriangleList,
-                strip_index_format: None,
-                front_face: wgpu::FrontFace::Ccw,
-                cull_mode: Some(wgpu::Face::Back),
-                // Setting this to anything other than Fill requires Features::NON_FILL_POLYGON_MODE
-                polygon_mode: wgpu::PolygonMode::Fill,
-                // Requires Features::DEPTH_CLAMPING
-                clamp_depth: false,
-                // Requires Features::CONSERVATIVE_RASTERIZATION
-                conservative: false,
-            },
-            depth_stencil: None,
-            multisample: wgpu::MultisampleState {
-                count: 1,
-                mask: !0,
-                alpha_to_coverage_enabled: false,
-            }});
-        Self{surface, device, queue, sc_desc, swap_chain, size, render_pipeline}
+        let pathbuf = fragment_path.to_path_buf();
+        let render_pipeline = create_pipeline(&device, &vertex_shader, &render_pipeline_layout, swapchain_format, &pathbuf).unwrap();
+        let validator = Validator::new(ValidationFlags::all(), Capabilities::all());
+        Self{surface, device, queue, sc_desc, swap_chain, size, render_pipeline, validator, fragment_path: pathbuf, vertex_shader}
     }
 
     fn render(&mut self) -> Result<(), wgpu::SwapChainError> {
@@ -139,11 +109,47 @@ impl State {
     }
 }
 
+fn create_pipeline(
+    device: &wgpu::Device,
+    vertex_shader: &wgpu::ShaderModule,
+    pipeline_layout: &wgpu::PipelineLayout,
+    swapchain_format: wgpu::TextureFormat,
+    fragment_path: &PathBuf,
+) -> Result<wgpu::RenderPipeline, String> {
+    let fragment_content = fs::read_to_string(fragment_path).unwrap();
+    let module = naga::front::wgsl::parse_str(&fragment_content).map_err(|e| format!("Parse Error: {}", &e))?;
+    Validator::new(ValidationFlags::all(), Capabilities::all()).validate(&module).map_err(|e| format!("Validation error: {}", &e))?;
+
+    let fragment_shader = device.create_shader_module(&wgpu::ShaderModuleDescriptor {
+        label: Some("Fragment shader"),
+        source: wgpu::ShaderSource::Wgsl(fragment_content.into()),
+        flags: wgpu::ShaderFlags::all()});
+
+    Ok(device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+        label: Some("Render Pipeline"),
+        layout: Some(&pipeline_layout),
+        vertex: wgpu::VertexState {
+            module: &vertex_shader,
+            entry_point: "main",
+            buffers: &[]},
+        fragment: Some(wgpu::FragmentState {
+            module: &fragment_shader,
+            entry_point: "main",
+            targets: &[wgpu::ColorTargetState {
+                format: swapchain_format,
+                blend: Some(wgpu::BlendState::REPLACE),
+                write_mask: wgpu::ColorWrite::ALL}]}),
+        primitive: wgpu::PrimitiveState::default(),
+        depth_stencil: None,
+        multisample: wgpu::MultisampleState{count: 1, mask: !0, alpha_to_coverage_enabled: false}})
+    )
+}
+
 fn main() {
     println!("Slim Shader!");
 
     let filename = std::env::args().nth(1).expect("no path to file fiven");
-    let path = PathBuf::from(filename);
+    let path = PathBuf::from(filename.as_str());
     println!("{:?}", path);
 
     let content = fs::read_to_string(path.to_str().unwrap()).expect("could not read file");
@@ -153,13 +159,14 @@ fn main() {
 
     let event_loop:EventLoop<ReloadEvent> = EventLoop::with_user_event();
     let window = WindowBuilder::new().build(&event_loop).unwrap();
-    let mut state = pollster::block_on(State::new(&window));
+    let mut state = pollster::block_on(State::new(&window, path.as_path()));
 
     {
+        let fragment_path = path.clone();
         std::thread::spawn(move || {
             let (tx, rx) = channel();
             let mut watcher: RecommendedWatcher = Watcher::new_raw(tx).unwrap();
-            watcher.watch(&path, notify::RecursiveMode::NonRecursive).unwrap();
+            watcher.watch(&fragment_path, notify::RecursiveMode::NonRecursive).unwrap();
             loop {
                 match rx.recv() {
                     Ok(RawEvent {path: Some(_), op: Ok(_), ..}) => println!("reload fragment shader"),
@@ -192,8 +199,7 @@ fn main() {
             Event::WindowEvent {ref event, window_id} if window_id == window.id() => if !state.input(event) {
                 match event {
                     WindowEvent::CloseRequested | WindowEvent::KeyboardInput {
-                        input:
-                            KeyboardInput {
+                        input: KeyboardInput {
                                 state: ElementState::Pressed,
                                 virtual_keycode: Some(VirtualKeyCode::Escape),
                                 ..
